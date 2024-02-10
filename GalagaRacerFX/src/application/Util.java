9793b5162cd811854ac5f80eb1e9d8505ac27691
yassir56069 <hoossanbukshyassir@gmail.com>
2024-02-10 20:25:33 +0400

    - Added Util Helper Class

---
+++ GalagaRacerFX/src/application/Util.java
@@ -0,0 +1,14 @@
+package application;
+
+public final class Util {
+
+    private Util(){}
+    
+    /**
+     * Return boolean of whether or not the value is even
+     * @param num
+     * @return
+     */
+    public static boolean isEven(double num) { return ((num % 2) == 0); }
+
+}
